4bb1fdd8b07bb016aefd22a5d7103ffb55228c48
heinNell <heinnell64@gmail.com>
2025-06-25 20:54:28 +0200

    Fix AppProvider import error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useAppContext } from "./context/AppContext";
+import { useAppContext, AppProvider } from "./context/AppContext";
 import ErrorBoundary from './components/ErrorBoundary';
 
 // UI Components
